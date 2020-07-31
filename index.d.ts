4848500b2d1130727a3960297aa6b0e5739c807c
SEdilson <edilson.silva00@hotmail.com>
2020-07-30 21:14:39 -0300

    Add types to lib

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+import passport = require('passport');
+import express = require('express');
+
+export interface Profile extends passport.Profile {
+  id: string;
+  fullName: string;
+  name: {
+    familyName: string;
+    givenName: string;
+  }
+  photos?: Array<{
+    value: string;
+  }>
+  token?: string;
+
+  _raw: string;
+  _json: any;
+}
+
+export interface StrategyOption {
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
+
+  authorizationURL?: string;
+  tokenURL?: string;
+  profileURL?: string;
+}
+
+
+export type VerifyFunction =
+  (accessToken: string, refreshToken: string, profile: Profile, done: (error: any, user?: any, info?: any) => void) => void;
+
+export class Strategy implements passport.Strategy {
+  constructor(options: StrategyOption, verify: VerifyFunction);
+
+  name: string;
+  authenticate(req: express.Request, options?: object): void;
+}
